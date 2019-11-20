c7e6c6109d9baa89d8ba1879e54b0a7eee1e24ac
orchoban <33927385+orchoban@users.noreply.github.com>
2019-11-20 17:40:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+ackage for [reco cli: react.cordova](https://www.npmjs.com/package/react.cordova)
---
+++ dist/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
+// [::1] is the IPv6 localhost address.
+window.location.hostname === '[::1]' ||
+// 127.0.0.1/8 is considered localhost for IPv4.
+window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/));
+
+var cordovaScript_URL = "";
+
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    //production
+} else {
+    cordovaScript_URL = document.URL.slice(0, document.URL.lastIndexOf(":")) + ":8597/browser/www";
+}
+
+window.addEventListener('load', function (e) {
+
+    var tag = document.createElement('script');
+    tag.async = true;
+    tag.src = cordovaScript_URL + process.env.PUBLIC_URL + '/cordova.js';
+
+    e.currentTarget.document.body.appendChild(document.createComment("This is an automatic switching src according to the run method. Comes from cordova_script"));
+    e.currentTarget.document.body.appendChild(tag);
+});
